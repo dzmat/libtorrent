202b572804e7f1ab73e495fd6b60e560a3836a29
dzmat <dzmat@yandex.ru>
2018-03-30 16:26:39 +0700

    build prepare
    # Conflicts:
    #	Jamfile

---
+++ bb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./b2 -q --without-python --toolset=gcc architecture=x86 address-model=32 variant=debug link=shared runtime-link=shared debug-symbols=on encryption=on crypto=openssl logging=off resolve-countries=off dht=on character-set=unicode boost-link=shared -sBOOST_ROOT="/c/work/qbt_build_tree/boost_1_68_0" include="/c/work/qbt_build_tree/base/include" library-path="/c/work/qbt_build_tree/base/lib" --prefix="/c/work/qbt_build_tree/base" define=BOOST_ASIO_DISABLE_CONNECTEX
